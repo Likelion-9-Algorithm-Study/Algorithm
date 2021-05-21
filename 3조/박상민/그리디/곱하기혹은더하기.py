5875cbea329a5d04f4425e434b5ea9ac06a77390
Sangmin Park <iamsangminpark@gmail.com>
2021-05-21 11:53:39 +0900

    코드 수정

--- 3조/박상민/그리디/곱하기혹은더하기.py
+++ 3조/박상민/그리디/곱하기혹은더하기.py
@@ -3,22 +3,13 @@
 a = input()
 b = list(map(int, list(str(a))))
 
-result = 0
+result = b[0]
 
-for i in range(len(b)):
-    if b[i] == 0:
-        continue
-    else:
-        if i == 0:
-            result = b[i]
-        elif i == 1:
-            if b[i-1] == 0:
-                result += b[i]
-            else:
-                result *= b[i]
-        else:
+for i in range(1, len(b)):
+    if b[i] != 0:
+        if result != 0:
             result *= b[i]
+        else:
+            result += b[i]
 
 print(result)
-
-# 시간 좀걸림
