0b7a0e0ee302d2f74da11e0de861483dc80fcd7f
Sangmin Park <iamsangminpark@gmail.com>
2021-10-07 19:01:44 +0900

    정렬 두문제

---
+++ 3조/박상민/정렬/센서.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+k = int(input())
+sensors = sorted(list(map(int, input().split())))
+
+diff = sorted([sensors[i]-sensors[i-1] for i in range(1, len(sensors))], reverse=True)
+if n >= k:
+    for _ in range(k-1):
+        diff.pop(0)
+else:
+    print(0)
+if n >= k:
+    print(sum(diff))
